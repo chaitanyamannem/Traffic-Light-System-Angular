035f61c4a0aae18b5cb372e42e02712fbac40802
devchaitu18 <devchaitu18@gmail.com>
2016-12-10 23:29:48 +0530

    Added home controller

--- home/home.js
+++ home/home.js
@@ -1,3 +1,21 @@
 angular.module('trafficLightSystem').component('home', {
-    templateUrl: '/home/home.html'
+    templateUrl: '/home/home.html',
+    controller: function () {
+
+        console.log("In Controller");
+
+        this.message = "Intiating the Traffic System";
+
+        this.startRedLight = function () {
+            this.message = "red";
+        };
+
+        this.startYellowLight = function () {
+            this.message = "yellow";
+        };
+
+        this.startGreenLight = function () {
+            this.message = "green";
+        };
+    }
 })
